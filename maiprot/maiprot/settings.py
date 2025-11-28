a1d86b4c16627142a3637a05190acb43cba0c1a0
Josevrider <142505686+Josevrider@users.noreply.github.com>
2025-11-28 04:04:29 +0000

    fix static files in production

--- maiprot/maiprot/settings.py
+++ maiprot/maiprot/settings.py
@@ -147,9 +147,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'appweb' / 'static']
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'appweb' / 'static',
+]
+
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'appweb/templates']
 LOGIN_URL = '/login/'
 LOGOUT_REDIRECT_URL = '/'
